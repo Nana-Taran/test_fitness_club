822adb99c1bf4da6f6f6b100d66e9ee69809554b
Nana-Taran <nt.kvadrant@gmail.com>
2025-09-12 22:27:00 +0200

    domain implemented

--- src/main/java/app/damain/Member.java
+++ src/main/java/app/damain/Member.java
@@ -8,6 +8,27 @@ public class Member {
     private  String name;
     private boolean active;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
